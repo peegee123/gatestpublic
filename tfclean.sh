fdb9ca948c9613d3a5e2e1cb93d8cfc398dc46ad
Peter Woods <peter.woods@uicentric.com>
2024-02-13 13:45:34 +0000

    mod

---
+++ tfclean.sh
@@ -0,0 +1,30 @@
+#!/bin/zsh
+
+ls -lart  terraform/config/
+
+
+ls -lart  terraform/config/.terraform || true
+ls -lart  terraform/config/.terraform.lock.hcl || true
+ls -lart  terraform/config/tfplan || true
+ls -lart  terraform/config/terraform.tfstate.backup || true
+ls -lart  terraform/config/terraform.tfstate || true
+ls -lart  terraform/config/.DS_Store || true
+
+
+rm -Rf terraform/config/.terraform || true
+rm -Rf terraform/config/.terraform.lock.hcl || true
+rm -Rf terraform/config/tfplan || true
+rm -Rf terraform/config/terraform.tfstate.backup || true
+rm -Rf terraform/config/terraform.tfstate || true
+rm -Rf terraform/config/.DS_Store || true
+
+
+ls -lart  terraform/config/.terraform || true
+ls -lart  terraform/config/.terraform.lock.hcl || true
+ls -lart  terraform/config/tfplan || true
+ls -lart  terraform/config/terraform.tfstate.backup || true
+ls -lart  terraform/config/terraform.tfstate || true
+
+
+ls -lart  terraform/config || true
+
