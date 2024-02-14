716e65c745eee8d6a4b97d9547ffdb38701f5a22
Peter Woods <peter.woods@uicentric.com>
2024-02-14 11:44:26 +0000

    clean up

--- tfclean.sh
+++ tfclean.sh
@@ -18,6 +18,8 @@ rm -Rf terraform/config/terraform.tfstate.backup || true
 rm -Rf terraform/config/terraform.tfstate || true
 rm -Rf terraform/config/.DS_Store || true
 rm -Rf terraform/config/tpf1.txt || true
+rm -Rf terraform/config/my_folder/
+
 
 # ls -lart  terraform/config/.terraform || true
 # ls -lart  terraform/config/.terraform.lock.hcl || true
