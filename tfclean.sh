6af497daea1744438086036b0a8ea11de81c46f9
Peter Woods <peter.woods@uicentric.com>
2024-02-14 11:23:18 +0000

    add example of building with approval

--- tfclean.sh
+++ tfclean.sh
@@ -1,14 +1,14 @@
 #!/bin/zsh
 
-ls -lart  terraform/config/
-
-
-ls -lart  terraform/config/.terraform || true
-ls -lart  terraform/config/.terraform.lock.hcl || true
-ls -lart  terraform/config/tfplan || true
-ls -lart  terraform/config/terraform.tfstate.backup || true
-ls -lart  terraform/config/terraform.tfstate || true
-ls -lart  terraform/config/.DS_Store || true
+# ls -lart  terraform/config/
+# 
+# 
+# ls -lart  terraform/config/.terraform || true
+# ls -lart  terraform/config/.terraform.lock.hcl || true
+# ls -lart  terraform/config/tfplan || true
+# ls -lart  terraform/config/terraform.tfstate.backup || true
+# ls -lart  terraform/config/terraform.tfstate || true
+# ls -lart  terraform/config/.DS_Store || true
 
 
 rm -Rf terraform/config/.terraform || true
@@ -17,14 +17,14 @@ rm -Rf terraform/config/tfplan || true
 rm -Rf terraform/config/terraform.tfstate.backup || true
 rm -Rf terraform/config/terraform.tfstate || true
 rm -Rf terraform/config/.DS_Store || true
+rm -Rf terraform/config/tpf1.txt || true
 
-
-ls -lart  terraform/config/.terraform || true
-ls -lart  terraform/config/.terraform.lock.hcl || true
-ls -lart  terraform/config/tfplan || true
-ls -lart  terraform/config/terraform.tfstate.backup || true
-ls -lart  terraform/config/terraform.tfstate || true
-
-
-ls -lart  terraform/config || true
-
+# ls -lart  terraform/config/.terraform || true
+# ls -lart  terraform/config/.terraform.lock.hcl || true
+# ls -lart  terraform/config/tfplan || true
+# ls -lart  terraform/config/terraform.tfstate.backup || true
+# ls -lart  terraform/config/terraform.tfstate || true
+# 
+# 
+# ls -lart  terraform/config || true					
+# 
